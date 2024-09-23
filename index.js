bbde2ea685e4c532b1387c73d417058b0ef05ad8
nyambogahezron <hezzyn1@gmail.com>
2024-09-23 14:42:30 +0300

    client init

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello, world!');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
+// server client
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
+const startServer = async () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
